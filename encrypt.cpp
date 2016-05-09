0ca046aaa0cb50167f7331c89128527bdbf15e91
Cleomedes <antwauntune25@wcpsstudent.org>
2016-05-09 08:46:27 -0400

    Create encrypt.cpp

---
+++ encrypt.cpp
@@ -0,0 +1,82 @@
+using System;
+using System.Windows.Forms;
+namespace OpenBrowser
+{
+
+          public class WebPage : System.Windows.Forms.Form
+          {
+ 
+                    private AxSHDocVw.AxWebBrowser axWebBrowser1;
+                    private System.Windows.Forms.Button browseButton;
+ 
+                    public WebPage ()
+                    {
+                             InitializeComponent();
+                    }
+ 
+                    private void InitializeComponent()
+                    {
+                             System.Resources.ResourceManager resources = new System.Resources.ResourceManager(typeof(WebPage));
+                             this.browseButton = new System.Windows.Forms.Button();
+                             this.axWebBrowser1 = new AxSHDocVw.AxWebBrowser();
+                             ((System.ComponentModel.ISupportInitialize)(this.axWebBrowser1)).BeginInit();
+                             this.SuspendLayout();
+ 
+                             //
+                             // browseButton
+                             //
+ 
+                             this.browseButton.Location = new System.Drawing.Point(8, 8);
+                             this.browseButton.Name = "browseButton";
+                             this.browseButton.TabIndex = 1;
+                             this.browseButton.Text = "Browse";
+                             this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+ 
+                             //
+                             // axWebBrowser1
+                             //
+ 
+                             this.axWebBrowser1.Enabled = true;
+                             this.axWebBrowser1.Location = new System.Drawing.Point(10, 40);
+                             this.axWebBrowser1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWebBrowser1.OcxState")));
+                             this.axWebBrowser1.Size = new System.Drawing.Size(980, 808);
+                             this.axWebBrowser1.TabIndex = 6;
+ 
+                             //
+                             // WebPage
+                             //
+ 
+                             this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
+                             this.ClientSize = new System.Drawing.Size(292, 266);
+                             this.Controls.Add(this.browseButton);
+                             this.Controls.Add(this.axWebBrowser1);
+                             this.Name = "WebPage";
+                             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+                             ((System.ComponentModel.ISupportInitialize)(this.axWebBrowser1)).EndInit();
+                             this.ResumeLayout(false);
+                    }
+ 
+                    /// <summary>
+                    /// The main entry point for the application.
+                    /// </summary>
+ 
+                    [STAThread]
+                    static void Main()
+                    {
+                             Application.Run(new WebPage ());
+                    }
+                    private void browseButton_Click(object sender, System.EventArgs e)
+                    {
+                             // Use Following code with version 2.0
+                             axWebBrowser1.Navigate(pathtofile);
+                             // Use Following code with version 1.1
+                             //System.Object nullObject = 0;
+                             //string str = "";
+                             //System.Object nullObjStr = str;
+                             //Cursor.Current = Cursors.WaitCursor;
+                             //axWebBrowser1.Navigate(@"pathtofile",ref nullObject, ref 
+                             nullObjStr, ref nullObjStr, ref nullObjStr);
+                             //Cursor.Current = Cursors.Default;
+                             }
+          }
+}
